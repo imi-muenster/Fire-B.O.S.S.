d34f6ec71cceb922a9ec0ee1beeefb26bf03fabf
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-05-02 16:17:51 +0200

    More testing functions

--- src/main/kotlin/de/uni_muenster/imi/fhirFacade/Test.kt
+++ src/main/kotlin/de/uni_muenster/imi/fhirFacade/Test.kt
@@ -1,7 +1,9 @@
 import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.rest.annotation.Search
 import de.uni_muenster.imi.fhirFacade.fhir.decodeFromString
+import de.uni_muenster.imi.fhirFacade.fhir.decodeQueryResults
 import de.uni_muenster.imi.fhirFacade.fhir.encodeFromResource
+import de.uni_muenster.imi.fhirFacade.fhir.incrementVersion
 import org.basex.api.client.ClientSession
 import org.basex.core.BaseXException
 import org.hl7.fhir.instance.model.api.IBaseResource
@@ -9,7 +11,6 @@ import org.hl7.fhir.r4.formats.ParserType
 import org.hl7.fhir.r4.model.*
 import java.io.File
 import java.math.BigDecimal
-import java.util.*
 
 
 private val ctx = FhirContext.forR4()
@@ -17,8 +18,7 @@ private val parser = ctx.newXmlParser().setPrettyPrint(true)
 
 
 fun main() {
-    //println("${Patient::class.simpleName}")
-    TestBasex.addPatient()
+    testQueryDecoding()
 }
 
 
@@ -80,7 +80,7 @@ object TestBasex {
 
     fun addPatient() {
         val text = File("src/main/resources/fhirResources/patient01").readText()
-        val resource = decodeFromString(text, ParserType.JSON)
+        val resource = decodeFromString(text)
         this.postResourceToBaseX(resource!!)
     }
 
@@ -89,10 +89,9 @@ object TestBasex {
         val id = resource.idElement.idPart
 
         if (!resource.idElement.hasVersionIdPart()) {
-            resource.meta.versionId = "2"
-            resource.meta.lastUpdated = Date(System.currentTimeMillis())
+            resource.incrementVersion()
         }
-        val version = resource.meta.versionId
+        val version = resource.idElement.versionIdPart
 
         encodeFromResource(resource)?.let {
             try {
@@ -110,4 +109,126 @@ object TestBasex {
 
 private fun printAsXml(resource: IBaseResource) {
     println(parser.encodeResourceToString(resource))
+}
+
+fun testQueryDecoding() {
+    val test = "<results>\n" +
+            "  <result>\n" +
+            "    <Patient>\n" +
+            "      <id value=\"pat2\"/>\n" +
+            "      <meta>\n" +
+            "        <versionId value=\"1\"/>\n" +
+            "        <lastUpdated value=\"2022-05-02T13:35:36.152+02:00\"/>\n" +
+            "      </meta>\n" +
+            "      <text>\n" +
+            "        <status value=\"generated\"/>\n" +
+            "        <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            "          <p>Patient Donald D DUCK @ Acme Healthcare, Inc. MR = 123456</p>\n" +
+            "        </div>\n" +
+            "      </text>\n" +
+            "      <identifier>\n" +
+            "        <use value=\"usual\"/>\n" +
+            "        <type>\n" +
+            "          <coding>\n" +
+            "            <system value=\"http://hl7.org/fhir/v2/0203\"/>\n" +
+            "            <code value=\"MR\"/>\n" +
+            "          </coding>\n" +
+            "        </type>\n" +
+            "        <system value=\"urn:oid:0.1.2.3.4.5.6.7\"/>\n" +
+            "        <value value=\"123456\"/>\n" +
+            "      </identifier>\n" +
+            "      <active value=\"true\"/>\n" +
+            "      <name>\n" +
+            "        <use value=\"official\"/>\n" +
+            "        <family value=\"Donald\"/>\n" +
+            "        <given value=\"Duck\"/>\n" +
+            "        <given value=\"D\"/>\n" +
+            "      </name>\n" +
+            "      <gender value=\"other\">\n" +
+            "        <extension url=\"http://example.org/Profile/administrative-status\">\n" +
+            "          <valueCodeableConcept>\n" +
+            "            <coding>\n" +
+            "              <system value=\"http://hl7.org/fhir/v2/0001\"/>\n" +
+            "              <code value=\"A\"/>\n" +
+            "              <display value=\"Ambiguous\"/>\n" +
+            "            </coding>\n" +
+            "          </valueCodeableConcept>\n" +
+            "        </extension>\n" +
+            "      </gender>\n" +
+            "      <photo>\n" +
+            "        <contentType value=\"image/gif\"/>\n" +
+            "        <data value=\"R0lGODlhEwARAPcAAAAAAAAA/+9aAO+1AP/WAP/eAP/eCP/eEP/eGP/nAP/nCP/nEP/nIf/nKf/nUv/nWv/vAP/vCP/vEP/vGP/vIf/vKf/vMf/vOf/vWv/vY//va//vjP/3c//3lP/3nP//tf//vf///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////yH5BAEAAAEALAAAAAATABEAAAi+AAMIDDCgYMGBCBMSvMCQ4QCFCQcwDBGCA4cLDyEGECDxAoAQHjxwyKhQAMeGIUOSJJjRpIAGDS5wCDly4AALFlYOgHlBwwOSNydM0AmzwYGjBi8IHWoTgQYORg8QIGDAwAKhESI8HIDgwQaRDI1WXXAhK9MBBzZ8/XDxQoUFZC9IiCBh6wEHGz6IbNuwQoSpWxEgyLCXL8O/gAnylNlW6AUEBRIL7Og3KwQIiCXb9HsZQoIEUzUjNEiaNMKAAAA7\"/>\n" +
+            "      </photo>\n" +
+            "      <managingOrganization>\n" +
+            "        <reference value=\"Organization/1\"/>\n" +
+            "        <display value=\"ACME Healthcare, Inc\"/>\n" +
+            "      </managingOrganization>\n" +
+            "      <link>\n" +
+            "        <other>\n" +
+            "          <reference value=\"Patient/pat1\"/>\n" +
+            "        </other>\n" +
+            "        <type value=\"seealso\"/>\n" +
+            "      </link>\n" +
+            "    </Patient>\n" +
+            "  </result>\n" +
+            "  <result>\n" +
+            "    <Patient>\n" +
+            "      <id value=\"pat2\"/>\n" +
+            "      <meta>\n" +
+            "        <versionId value=\"2\"/>\n" +
+            "        <lastUpdated value=\"2022-05-02T13:35:36.152+02:00\"/>\n" +
+            "      </meta>\n" +
+            "      <text>\n" +
+            "        <status value=\"generated\"/>\n" +
+            "        <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            "          <p>Patient Donald D DUCK @ Acme Healthcare, Inc. MR = 123456</p>\n" +
+            "        </div>\n" +
+            "      </text>\n" +
+            "      <identifier>\n" +
+            "        <use value=\"usual\"/>\n" +
+            "        <type>\n" +
+            "          <coding>\n" +
+            "            <system value=\"http://hl7.org/fhir/v2/0203\"/>\n" +
+            "            <code value=\"MR\"/>\n" +
+            "          </coding>\n" +
+            "        </type>\n" +
+            "        <system value=\"urn:oid:0.1.2.3.4.5.6.7\"/>\n" +
+            "        <value value=\"123456\"/>\n" +
+            "      </identifier>\n" +
+            "      <active value=\"true\"/>\n" +
+            "      <name>\n" +
+            "        <use value=\"official\"/>\n" +
+            "        <family value=\"Donald\"/>\n" +
+            "        <given value=\"Duck\"/>\n" +
+            "        <given value=\"D\"/>\n" +
+            "      </name>\n" +
+            "      <gender value=\"other\">\n" +
+            "        <extension url=\"http://example.org/Profile/administrative-status\">\n" +
+            "          <valueCodeableConcept>\n" +
+            "            <coding>\n" +
+            "              <system value=\"http://hl7.org/fhir/v2/0001\"/>\n" +
+            "              <code value=\"A\"/>\n" +
+            "              <display value=\"Ambiguous\"/>\n" +
+            "            </coding>\n" +
+            "          </valueCodeableConcept>\n" +
+            "        </extension>\n" +
+            "      </gender>\n" +
+            "      <photo>\n" +
+            "        <contentType value=\"image/gif\"/>\n" +
+            "        <data value=\"R0lGODlhEwARAPcAAAAAAAAA/+9aAO+1AP/WAP/eAP/eCP/eEP/eGP/nAP/nCP/nEP/nIf/nKf/nUv/nWv/vAP/vCP/vEP/vGP/vIf/vKf/vMf/vOf/vWv/vY//va//vjP/3c//3lP/3nP//tf//vf///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////yH5BAEAAAEALAAAAAATABEAAAi+AAMIDDCgYMGBCBMSvMCQ4QCFCQcwDBGCA4cLDyEGECDxAoAQHjxwyKhQAMeGIUOSJJjRpIAGDS5wCDly4AALFlYOgHlBwwOSNydM0AmzwYGjBi8IHWoTgQYORg8QIGDAwAKhESI8HIDgwQaRDI1WXXAhK9MBBzZ8/XDxQoUFZC9IiCBh6wEHGz6IbNuwQoSpWxEgyLCXL8O/gAnylNlW6AUEBRIL7Og3KwQIiCXb9HsZQoIEUzUjNEiaNMKAAAA7\"/>\n" +
+            "      </photo>\n" +
+            "      <managingOrganization>\n" +
+            "        <reference value=\"Organization/1\"/>\n" +
+            "        <display value=\"ACME Healthcare, Inc\"/>\n" +
+            "      </managingOrganization>\n" +
+            "      <link>\n" +
+            "        <other>\n" +
+            "          <reference value=\"Patient/pat1\"/>\n" +
+            "        </other>\n" +
+            "        <type value=\"seealso\"/>\n" +
+            "      </link>\n" +
+            "    </Patient>\n" +
+            "  </result>\n" +
+            "</results>"
+    val results = decodeQueryResults(test)
 }
