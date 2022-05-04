281d7e1e263f187f603b11411ad3a399fa28615c
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-05-04 16:02:36 +0200

    Implemented UUID-Generator based on time

---
+++ src/main/kotlin/de/uni_muenster/imi/fhirFacade/utils/UUIDHelper.kt
@@ -0,0 +1,27 @@
+package de.uni_muenster.imi.fhirFacade.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+object UUIDHelper {
+
+    private const val MIN_SEQ = 0
+    private const val MAX_SEQ = 9
+
+    private var lastDate = SimpleDateFormat("yyMMddHHmmss").format(Date())
+    private var lastSequence = 0
+
+    fun getUID(): String {
+        val currentDateTime = SimpleDateFormat("yyMMddHHmmss").format(Date())
+        if (currentDateTime == lastDate) {
+            lastSequence++
+        } else {
+            lastDate = currentDateTime
+            lastSequence = MIN_SEQ
+        }
+        check(lastSequence <= MAX_SEQ) { "Sequence numbers out of range.!" }
+        return lastDate + lastSequence
+    }
+
+}
