a36096d58caabb827b662b1bee7fa6b445b8c9e6
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-05-23 09:19:47 +0200

    Added milliseconds to UUID

--- src/main/kotlin/de/uni_muenster/imi/fhirFacade/utils/UUIDHelper.kt
+++ src/main/kotlin/de/uni_muenster/imi/fhirFacade/utils/UUIDHelper.kt
@@ -9,11 +9,11 @@ object UUIDHelper {
     private const val MIN_SEQ = 0
     private const val MAX_SEQ = 9
 
-    private var lastDate = SimpleDateFormat("yyMMddHHmmss").format(Date())
+    private var lastDate = SimpleDateFormat("yyMMddHHmmssSSS").format(Date())
     private var lastSequence = 0
 
     fun getUID(): String {
-        val currentDateTime = SimpleDateFormat("yyMMddHHmmss").format(Date())
+        val currentDateTime = SimpleDateFormat("yyMMddHHmmssSSS").format(Date())
         if (currentDateTime == lastDate) {
             lastSequence++
         } else {
@@ -21,7 +21,7 @@ object UUIDHelper {
             lastSequence = MIN_SEQ
         }
         check(lastSequence <= MAX_SEQ) { "Sequence numbers out of range.!" }
-        return lastDate + lastSequence
+        return "$lastDate$lastSequence"
     }
 
 }
