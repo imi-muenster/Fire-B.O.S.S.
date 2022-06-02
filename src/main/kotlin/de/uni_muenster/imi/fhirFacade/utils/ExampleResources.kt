6e9d89a4b16d055d0bb8e08abc6d6d85521ff589
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-06-02 13:42:05 +0200

    Implemented method to add bulk of example resources to server

---
+++ src/main/kotlin/de/uni_muenster/imi/fhirFacade/utils/ExampleResources.kt
@@ -0,0 +1,39 @@
+package de.uni_muenster.imi.fhirFacade.utils
+
+import de.uni_muenster.imi.fhirFacade.fhir.helper.decodeFromString
+import de.uni_muenster.imi.fhirFacade.fhir.helper.encodeFromResource
+import org.hl7.fhir.instance.model.api.IBaseResource
+import org.hl7.fhir.r4.formats.ParserType
+import java.io.*
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main() {
+    fillServer()
+}
+
+fun fillServer() {
+    val files = File("src/main/resources/fhirResources/examples-json")
+    for (file in files.listFiles()!!) {
+        val resource = decodeFromString(file.readText())
+        if (resource != null) {
+            addToServer(resource)
+        }
+    }
+}
+
+fun addToServer(resource: IBaseResource) {
+    val body = encodeFromResource(resource, ParserType.JSON)
+
+    val client = HttpClient.newBuilder().build()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("http://localhost:8080/fhir/${resource.fhirType()}"))
+        .POST(HttpRequest.BodyPublishers.ofString(body))
+        .setHeader("Content-type", "application/json")
+        .build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    println(response.body())
+
+}
