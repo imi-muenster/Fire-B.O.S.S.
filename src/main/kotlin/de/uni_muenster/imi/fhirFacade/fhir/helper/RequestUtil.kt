fa0ca62657f519a9dd06aaa994e3e91a6dbf0d51
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-08-02 09:07:23 +0200

    Implemented RequestUtil to perform interim queries

---
+++ src/main/kotlin/de/uni_muenster/imi/fhirFacade/fhir/helper/RequestUtil.kt
@@ -0,0 +1,16 @@
+package de.uni_muenster.imi.fhirFacade.fhir.helper
+
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class RequestUtil {
+
+    fun performQuery(theRequest: HttpRequest): HttpResponse<String>? {
+        val client = HttpClient.newBuilder().build()
+
+        val response = client.send(theRequest, HttpResponse.BodyHandlers.ofString())
+        return response
+    }
+
+}
