2b241cf0301e2c23a93703d8e889c3e858ce348d
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-08-23 16:04:11 +0200

    Changed Settings Util to support relative paths

--- src/main/kotlin/de/uni_muenster/imi/fhirFacade/utils/SettingsUtil.kt
+++ src/main/kotlin/de/uni_muenster/imi/fhirFacade/utils/SettingsUtil.kt
@@ -1,15 +1,23 @@
 package de.uni_muenster.imi.fhirFacade.utils
 
+import com.apicatalog.jsonld.loader.FileLoader
 import java.io.File
 import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.net.URI
 import javax.servlet.ServletContext
 
-class Properties(context: ServletContext, path: String) {
+class Properties @JvmOverloads constructor(context: ServletContext? = null, path: String) {
 
-    val file = File(context.getRealPath(path))
-    private val props = java.util.Properties()
-
+    private val props: java.util.Properties
+    private val file: File
     init {
+        if (context != null) {
+            file = File(context.getRealPath(path))
+        } else {
+            file = File(path)
+        }
+        props = java.util.Properties()
         props.load(FileInputStream(file))
     }
 
